2afe578498e94bd5d8046d9e3cd95bfaff42aa4b
guiyobr <barandaguillermo@gmail.com>
2026-04-13 14:35:25 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const PORT = 8787;
+const PORT = process.env.PORT || 8787;
 
 app.use(cors());
 app.use(express.json());
